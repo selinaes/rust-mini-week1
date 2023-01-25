d9fa889a73222623a47f5e2d0e531c743134094b
Jiawei Liu (Selina) <selinaslytherin@126.com>
2023-01-25 18:07:29 +0000

    finally able to use copilot, added 4th test

--- tests/cli.rs
+++ tests/cli.rs
@@ -27,3 +27,11 @@ fn test1994() {
     cmd.assert().success().stdout("MCMXCIV\n");
 
 }
+
+#[test]
+//a test to invoke the cli with an subcommand 'convert' and option --num 2004
+fn test2004() {
+    let mut cmd = Command::cargo_bin("mini1").unwrap();
+    cmd.arg("convert").arg("--num").arg("2004");
+    cmd.assert().success().stdout("MMIV\n");
+}
